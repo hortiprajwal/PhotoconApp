da213c433357c59e941cb1da28a35d98aba2e93a
Prajwal Horti <58554461+hortiprajwal@users.noreply.github.com>
2022-07-21 09:53:43 +0530

    Add files via upload

---
+++ client/src/components/Form/styles.js
@@ -0,0 +1,30 @@
+
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+    },
+  },
+  paper: {
+    padding: theme.spacing(2),
+  },
+  log: {
+    padding: theme.spacing(2),
+    background: "rgb(18,147,255)",
+    color: "white",
+  },
+  form: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  fileInput: {
+    width: '97%',
+    margin: '10px 0',
+  },
+  buttonSubmit: {
+    marginBottom: 10,
+  },
+}));
